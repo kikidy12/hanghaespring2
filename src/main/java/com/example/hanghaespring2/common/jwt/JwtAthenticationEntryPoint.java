a5c53b1cc866dfe721377c14e101555624ccb791
kikidy12 <kikidy12@naver.com>
2023-02-11 17:26:05 +0900

    jwt security 에러 처리

---
+++ src/main/java/com/example/hanghaespring2/common/jwt/JwtAthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.example.hanghaespring2.common.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.info("UnAuthorizaed!!! message : " + authException.getMessage());
+        //response에 넣기
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
