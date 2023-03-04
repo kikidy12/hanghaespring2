ee4bd86d1d613001251cf96473116687813bee83
kikidy12 <kikidy12@naver.com>
2023-03-04 21:03:29 +0900

    temp

--- src/main/java/com/example/hanghaespring2/common/dto/ErrorCode.java
+++ src/main/java/com/example/hanghaespring2/common/dto/ErrorCode.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
+
+
+    //sdfdsfdsf
     NOT_NULL(400,"필수값이 누락되었습니다"),
     PATTERN(400, "잘못된 양식의 값이 있습니다."),
     TOKEN_AUTH_ERROR(400, "토큰 인증 오류"),
